1eee32d83baa3a08fff72c6915efe97a79e202e9
Juanpi194 <juanpiviz194@gmail.com>
2026-08-04 16:25:23 +0200

    Pacific factory

--- inc/factories/NpcPacificFactory.hpp
+++ inc/factories/NpcPacificFactory.hpp
@@ -2,16 +2,33 @@
 #include <string>
 
 #include "utils/attributes.hpp"
+#include "characters/QuestGiver.hpp"
 
 class NPC;
 class Enchanter;
+class Merchant;
+class Narrator;
 
 class NpcPacificFactory
 {
 	public:
-		static 	NPC		*create_from_name(const std::string& npc_name) TAP_RETURNS_NONNULL TAP_UNUSED_RESULT;
+		// Specific npcs ------------------------------------------------------
 
-		// Specific enemies ---------------------------------------------------
+		// Enchanter --
 
-		static Enchanter	*create_enchanter(void) noexcept TAP_RETURNS_NONNULL TAP_UNUSED_RESULT;
+		static Enchanter	*create_enchanter_basic(const std::string& name, const std::string& description) noexcept TAP_RETURNS_NONNULL TAP_UNUSED_RESULT;
+		static Enchanter	*create_enchanter_advanced(const std::string& name, const std::string& description) noexcept TAP_RETURNS_NONNULL TAP_UNUSED_RESULT;
+
+		// Merchant --
+
+		static Merchant		*create_merchant_basic(const std::string& name, const std::string& description) noexcept TAP_RETURNS_NONNULL TAP_UNUSED_RESULT;
+		static Merchant		*create_merchant_advanced(const std::string& name, const std::string& description) noexcept TAP_RETURNS_NONNULL TAP_UNUSED_RESULT;
+
+		// Narrator --
+
+		static Narrator		*create_narrator(const std::string& name, const std::string& description, const std::string& sentence) noexcept TAP_RETURNS_NONNULL TAP_UNUSED_RESULT;
+
+		// Quest giver --
+
+		static QuestGiver	*create_quest_giver(const std::string& name, const std::string& description, Quest& quest, t_quest_dialogues dialogues) noexcept TAP_RETURNS_NONNULL TAP_UNUSED_RESULT;
 };
--- src/factories/NpcPacificFactory.cpp
+++ src/factories/NpcPacificFactory.cpp
@@ -1,25 +1,77 @@
 #include "factories/NpcPacificFactory.hpp"
 
 #include <stdexcept>
+#include <map>
 
 #include "utils/utils.hpp"
-#include "characters/enemies/Goblin.hpp"
-
-NPC	*NpcPacificFactory::create_from_name(const std::string& npc_name)
-{
-	// ? REVIEW: Should trim_str be used?
-	std::string	temp = npc_name;
-
-	trim_str(temp);
-	if (temp == Enchanter::NAME)
-		return (create_enchanter());
-	// TODO: Add more enemies.
-	throw std::invalid_argument("No enemy matches the name '" + npc_name + "'.");
-}
+#include "characters/Enchanter.hpp"
+#include "characters/Merchant.hpp"
+#include "characters/Narrator.hpp"
+#include "characters/QuestGiver.hpp"
+#include "enchantments/Enchantment.hpp"
+#include "enchantments/Flame.hpp"
+#include "enchantments/Freeze.hpp"
+#include "factories/EnchantmentFactory.hpp"
+#include "factories/ItemFactory.hpp"
+#include "items/consumables/Apple.hpp"
+#include "items/armor/BronzeArmor.hpp"
 
 // Specific enemies -----------------------------------------------------------
 
-Goblin	*NpcFactory::create_enchanter(void) noexcept
+// Enchanter --
+
+Enchanter	*NpcPacificFactory::create_enchanter_basic(const std::string& name, const std::string& description) noexcept
 {
-	return (new Enchanter());
+	std::map<Enchantment*, unsigned int>	pool;
+
+	// TODO: Other enchantments
+	pool[EnchantmentFactory::create_flame()] = 50;
+	pool[EnchantmentFactory::create_freeze()] = 80;
+	return (new Enchanter(name, description, pool));
+}
+
+Enchanter	*NpcPacificFactory::create_enchanter_advanced(const std::string& name, const std::string& description) noexcept
+{
+	std::map<Enchantment*, unsigned int>	pool;
+
+	// TODO: Other enchantments
+	pool[EnchantmentFactory::create_flame()] = 50;
+	pool[EnchantmentFactory::create_freeze()] = 80;
+	return (new Enchanter(name, description, pool));
+}
+
+// Merchant --
+
+Merchant	*NpcPacificFactory::create_merchant_basic(const std::string& name, const std::string& description) noexcept
+{
+	std::map<Item*, unsigned int>	pool;
+
+	// TODO: Other items
+	pool[ItemFactory::create_apple()] = 5;
+	pool[ItemFactory::create_bronze_armor()] = 30;
+	return (new Merchant(name, description, pool));
+}
+
+Merchant	*NpcPacificFactory::create_merchant_advanced(const std::string& name, const std::string& description) noexcept
+{
+	std::map<Item*, unsigned int>	pool;
+
+	// TODO: Other items
+	pool[ItemFactory::create_apple()] = 5;
+	pool[ItemFactory::create_bronze_armor()] = 30;
+	return (new Merchant(name, description, pool));
+}
+
+// Narrator --
+
+Narrator	*NpcPacificFactory::create_narrator(const std::string& name, const std::string& description, const std::string& sentence) noexcept
+{
+	return (new Narrator(name, description, sentence));
+}
+
+// Quest giver --
+
+QuestGiver	*NpcPacificFactory::create_quest_giver(const std::string& name, const std::string& description, Quest& quest, t_quest_dialogues dialogues) noexcept
+{
+	return (new QuestGiver(name, description, quest, dialogues));
 }
